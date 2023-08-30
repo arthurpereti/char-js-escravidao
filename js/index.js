da6fed1063bcc8f20b8fed67e80cfb4516726f3f
Arthur Pereti <arthurcoelho2005@outlook.com>
2023-08-30 17:58:57 -0300

    Aba SOBRE adicionada

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const tresbarrinhasmenu = document.getElementById('tresbarrinhasmenu')
-const mediaquerycabecalhocelular = document.getElementById('mediaquerycabecalhocelular')
-
-tresbarrinhasmenu.addEventListener('click', ()=>{
-    mediaquerycabecalhocelular.style.display = "inline";
-})
